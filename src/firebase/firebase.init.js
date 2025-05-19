674a3b0e902fb9abf3a808a3e8f6d6f0d83f0fad
Shohel-Raj <shohelraj485183@gmail.comgit config --global user.name Shohel-Rajgit config -lgit commit -m first>
2025-05-19 18:55:53 +0600

    enviroment variable added for ensure security

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,15 +6,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDt3KNEqh-CXOo4mqcsbfTQR6Fsa7mpkfk",
-  authDomain: "auth-assignment-project.firebaseapp.com",
-  projectId: "auth-assignment-project",
-  storageBucket: "auth-assignment-project.firebasestorage.app",
-  messagingSenderId: "85310773233",
-  appId: "1:85310773233:web:1c800cc7a41d1565291650"
+  apiKey:  import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId:  import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export  const auth =getAuth(app)
+export const auth = getAuth(app)
