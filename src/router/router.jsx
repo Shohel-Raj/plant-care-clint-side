f0f30fbdedecc8978f18d0f836197d53daf2c133
Shohel-Raj <shohelraj485183@gmail.comgit config --global user.name Shohel-Rajgit config -lgit commit -m first>
2025-05-19 19:35:37 +0600

    Navbar and Error page added

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router";
 import RootLayout from "../RootLayout/RootLayout";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
+import Error from "../Component/Error";
 
 const router = createBrowserRouter([
     {
       path: "/",
       Component:RootLayout,
+      errorElement:<Error></Error>,
       children:[
         {
             index:true,
